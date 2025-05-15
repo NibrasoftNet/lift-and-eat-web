61e6a0fadf1aeb5400dc73a3f7f6b0aa72b2bd9a
SofienRogue <sofien.abidi@digitalroguewave.com>
2025-05-15 18:37:47 +0100

    fix: increase image size

--- utlis/animations.ts
+++ utlis/animations.ts
@@ -64,7 +64,7 @@ export const createFadeInAnimation = (element: string) => {
         ease: "power2.out",
         scrollTrigger: {
           trigger: el,
-          start: "top 80%",
+          start: "top 50%",
           end: "bottom 20%",
           toggleActions: "play none none reverse",
         },
@@ -96,7 +96,7 @@ export const createSlideInAnimation = (
         ease: "power2.out",
         scrollTrigger: {
           trigger: el,
-          start: "top 80%",
+          start: "top 50%",
           end: "bottom 20%",
           toggleActions: "play none none reverse",
         },
